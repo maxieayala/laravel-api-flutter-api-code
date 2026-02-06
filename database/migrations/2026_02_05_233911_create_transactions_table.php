dd31f18d649962aae96f3f4aea60a190a467e41e
maxieayala <ing.ayoroz@gmail.com>
2026-02-06 09:34:57 -0600

    feat: Implement transaction management with API endpoints, model, requests, resource, and migration.

--- database/migrations/2026_02_05_233911_create_transactions_table.php
+++ database/migrations/2026_02_05_233911_create_transactions_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained();
+            $table->foreignId('user_id')->nullable()->constrained();
+            $table->date('transaction_date');
+            $table->integer('amount');
+            $table->string('description');
             $table->timestamps();
         });
     }
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\TransactionController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::apiResource('categories', CategoryController::class);
+Route::apiResource('transactions', TransactionController::class);
